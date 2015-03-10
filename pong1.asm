e14fd0acc6ed0c1cf7723060a60f595cb19c1876
Erik Andrén Zachrisson <erik@zachrisson.info>
2015-03-10 16:48:47 +0100

    Update comment

--- pong1.asm
+++ pong1.asm
@@ -132,7 +132,7 @@ LoadPalettesLoop:
   LDA #%10010000   ; enable NMI, sprites from Pattern Table 0, background from Pattern Table 1
   STA $2000
 
-  LDA #%00011110   ; enable sprites, enable background, no clipping on left side
+  LDA #%00011110   ; enable sprites, disable background, no clipping on left side
   STA $2001
 
 Forever:
